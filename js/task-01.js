70abd1983e86a097f5e23be67d0fcd0091607b73
Vadym4e <vadym.o92@gmail.com>
2023-02-12 16:39:45 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,11 @@
 const ulEl = document.querySelectorAll(".item");
+
 console.log("Number of categories:", ulEl.length);
+
 ulEl.forEach((element) => {
   const hEl = element.querySelector("h2");
   console.log("Category:", hEl.textContent);
+
   const liEl = element.querySelectorAll("li");
   console.log("Elements:", liEl.length);
 });
