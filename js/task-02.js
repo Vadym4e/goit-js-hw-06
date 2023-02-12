d6e4c5412820964b31cee97251fb3d1778c06808
Vadym4e <vadym.o92@gmail.com>
2023-02-12 17:51:34 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const elements = ingredients.map((element) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = element;
+  return liEl;
+});
+
+const ulEl = document.querySelector("ul");
+
+ulEl.append(...elements);
