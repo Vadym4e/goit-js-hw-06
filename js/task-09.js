97ff1500b68389c0e0dd1972994d58151d126f4d
Vadym4e <vadym.o92@gmail.com>
2023-02-13 17:03:53 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const bodyEl = document.querySelector("body");
+const colorName = document.querySelector(".color");
+const buttonEl = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+buttonEl.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+});
