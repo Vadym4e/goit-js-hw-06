1bf9c2af5ff10cb52b89d7a3e21e36edf5c3bb2d
Vadym4e <vadym.o92@gmail.com>
2023-02-13 16:31:53 +0200

    task - 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("всі поля повинні бути заповнені");
+  }
+
+  const formValues = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formValues);
+
+  event.currentTarget.reset();
+}
